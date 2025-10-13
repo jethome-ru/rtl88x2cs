2519c0b865e31d93daa65fc5b1168722f6ec7526
Viacheslav Bocharov <adeep@lexina.in>
2025-10-13 15:48:57 +0300

    core/crypto/sha256.h: fix for kernel 6.18

--- core/crypto/sha256.h
+++ core/crypto/sha256.h
@@ -15,8 +15,10 @@
 int hmac_sha256_vector(const u8 *key, size_t key_len, size_t num_elem,
 		       const u8 *addr[], const size_t *len, u8 *mac);
 #endif
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 18, 0))
 int hmac_sha256(const u8 *key, size_t key_len, const u8 *data,
 		size_t data_len, u8 *mac);
+#endif
 int sha256_prf(const u8 *key, size_t key_len, const char *label,
 	       const u8 *data, size_t data_len, u8 *buf, size_t buf_len);
 int sha256_prf_bits(const u8 *key, size_t key_len, const char *label,
