17ea4e55f2fe221af5cd632069f33270a14496b7
Igor Velkov <325961+iav@users.noreply.github.com>
2026-08-16 16:19:27 +0300

    proc: read only what was written in the sa_query / tx_deauth / tx_auth handlers
    The three handlers copied sizeof(tmp) (16) bytes from user space even
    though they had just limited count to 2, so a short write near the end
    of a page could fault on bytes the caller never passed. On a failed copy
    they carried on with key_type / tx_auth uninitialised and, for
    tx_deauth, sent a deauth with that value as the reason.
    
    Copy count bytes, terminate, and return -EFAULT when the copy fails.

--- core/rtw_debug.c
+++ core/rtw_debug.c
@@ -6136,16 +6136,15 @@ ssize_t proc_set_tx_sa_query(struct file *file, const char __user *buffer, size_
 		return -EFAULT;
 	}
 
-	if (buffer && !copy_from_user(tmp, buffer, sizeof(tmp))) {
+	if (!buffer || copy_from_user(tmp, buffer, count))
+		return -EFAULT;
+	tmp[count] = '\0';
 
-		int num = sscanf(tmp, "%x", &key_type);
-
-		if (num !=  1) {
-			RTW_INFO("invalid read_reg parameter!\n");
-			return count;
-		}
-		RTW_INFO("0: set sa query request , key_type=%d\n", key_type);
+	if (sscanf(tmp, "%x", &key_type) != 1) {
+		RTW_INFO("invalid read_reg parameter!\n");
+		return count;
 	}
+	RTW_INFO("0: set sa query request , key_type=%d\n", key_type);
 
 	if ((check_fwstate(pmlmepriv, WIFI_STATION_STATE) == _TRUE)
 	    && (check_fwstate(pmlmepriv, WIFI_ASOC_STATE) == _TRUE) && SEC_IS_BIP_KEY_INSTALLED(&padapter->securitypriv) == _TRUE) {
@@ -6217,16 +6216,15 @@ ssize_t proc_set_tx_deauth(struct file *file, const char __user *buffer, size_t 
 		return -EFAULT;
 	}
 
-	if (buffer && !copy_from_user(tmp, buffer, sizeof(tmp))) {
+	if (!buffer || copy_from_user(tmp, buffer, count))
+		return -EFAULT;
+	tmp[count] = '\0';
 
-		int num = sscanf(tmp, "%x", &key_type);
-
-		if (num !=  1) {
-			RTW_INFO("invalid read_reg parameter!\n");
-			return count;
-		}
-		RTW_INFO("key_type=%d\n", key_type);
+	if (sscanf(tmp, "%x", &key_type) != 1) {
+		RTW_INFO("invalid read_reg parameter!\n");
+		return count;
 	}
+	RTW_INFO("key_type=%d\n", key_type);
 	if (key_type < 0 || key_type > 4)
 		return count;
 
@@ -6325,16 +6323,15 @@ ssize_t proc_set_tx_auth(struct file *file, const char __user *buffer, size_t co
 		return -EFAULT;
 	}
 
-	if (buffer && !copy_from_user(tmp, buffer, sizeof(tmp))) {
+	if (!buffer || copy_from_user(tmp, buffer, count))
+		return -EFAULT;
+	tmp[count] = '\0';
 
-		int num = sscanf(tmp, "%x", &tx_auth);
-
-		if (num !=  1) {
-			RTW_INFO("invalid read_reg parameter!\n");
-			return count;
-		}
-		RTW_INFO("1: setnd auth, 2: send assoc request. tx_auth=%d\n", tx_auth);
+	if (sscanf(tmp, "%x", &tx_auth) != 1) {
+		RTW_INFO("invalid read_reg parameter!\n");
+		return count;
 	}
+	RTW_INFO("1: setnd auth, 2: send assoc request. tx_auth=%d\n", tx_auth);
 
 	if ((check_fwstate(pmlmepriv, WIFI_STATION_STATE) == _TRUE)
 	    && (check_fwstate(pmlmepriv, WIFI_ASOC_STATE) == _TRUE)) {
